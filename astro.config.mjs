3bd30216e8f6f9b1203d750dcd9034e39c81d357
dylancode <docs@dylancode.dev>
2026-02-24 12:51:39 +0000

    create stratified-sampling

--- astro.config.mjs
+++ astro.config.mjs
@@ -433,6 +433,7 @@ export default defineConfig({
 				    'simple-random-sampling',
 				    'systematic-sampling',
 				    'opportunity-sampling',
+				    'stratified-sampling',
 				]
 			    }
 			]
