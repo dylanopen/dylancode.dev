9c5ebcedaa2ce5e3d721d0670b8ed52aec4cb830
dylancode <opendylan@proton.me>
2026-03-15 21:22:28 +0000

    fix broken angle brackets in latex

--- build/js/tex.js
+++ build/js/tex.js
@@ -1,11 +1,11 @@
 for (let el of document.getElementsByTagName("ma")) {
-    katex.render(el.innerHTML, el, {
+    katex.render(el.innerText, el, {
 	throwOnError: false,
 	displayMode: true,
     })
 }
 for (let el of document.getElementsByTagName("im")) {
-    katex.render(el.innerHTML, el, {
+    katex.render(el.innerText, el, {
 	throwOnError: false,
 	displayMode: false,
     })
